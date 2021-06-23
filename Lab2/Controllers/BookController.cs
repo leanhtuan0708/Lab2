9238f4f27b5dd05263d7748a0f9c0133f61d91a2
Leanhtuan0708 <bentele80000@gmail.com>
2021-06-23 17:03:04 +0700

    THLab2

---
+++ Lab2/Controllers/BookController.cs
@@ -0,0 +1,128 @@
+﻿using Lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public String HelloTeacher(String name) {
+            return "Hello Lê Anh Tuấn" +name;
+        }
+        public ActionResult ListBook()
+        {
+            var books = new List<string>();
+            books.Add("HTML5 & CSS3 The complete Manual - Author Name Book 1");
+            books.Add("HTML 5 & CSS3 Responsive web Design");
+            books.Add("Professional ASP.NET MVC5 Book");
+            ViewBag.Books = books;
+;           return View();
+        }
+        public ActionResult ListBookModel()
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The complete Manual","Author Name Book 1", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "HTML5 & CSS3 The complete Manua2", "Author Name Book 2", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "HTML5 & CSS3 The complete Manua3", "Author Name Book 3", "/Content/Images/book3.jpg"));
+            return View(books);
+        }
+        public ActionResult EditBook(int id) {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The complete Manual", "Author Name Book 1", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "HTML5 & CSS3 The complete Manua2", "Author Name Book 2", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "HTML5 & CSS3 The complete Manua3", "Author Name Book 3", "/Content/Images/book3.jpg"));
+            //check if not exit
+            Book book = new Book();
+            foreach(Book b in books)
+            {
+                if (b.Id == id) {
+                    book = b;
+                    break;
+                }
+            }
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [HttpPost, ActionName("EditBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditBook(int Id, string Title, string Author, string Imagecover)
+        {
+
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The complete Manual", "Author Name Book 1", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "HTML5 & CSS3 The complete Manua2", "Author Name Book 2", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "HTML5 & CSS3 The complete Manua3", "Author Name Book 3", "/Content/Images/book3.jpg"));
+            //check if not exit
+            if (Id == null)
+            {
+                return HttpNotFound();
+            }
+            foreach (Book b in books)
+            {
+                if (b.Id == Id)
+                {
+                    b.Title = Title;
+                    b.Author = Author;
+                    b.Imagecover = Imagecover;
+                    break;
+                }
+            }
+
+            return View("ListBookModel", books);
+        }
+        public ActionResult CreateBook() {
+            return View();
+        }
+        [HttpPost, ActionName("CreateBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact([Bind(Include = "Id,Title,Author,Imagecover")]Book book)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The complete Manual", "Author Name Book 1", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "HTML5 & CSS3 The complete Manua2", "Author Name Book 2", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "HTML5 & CSS3 The complete Manua3", "Author Name Book 3", "/Content/Images/book3.jpg"));
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    books.Add(book);
+                }
+            }
+            catch (Exception /* dex */) {
+            }
+            return View("ListBookModel", books);
+        }
+        public ActionResult DeleteBook() 
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteBook(int? Id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS3 The complete Manual", "Author Name Book 1", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "HTML5 & CSS3 The complete Manua2", "Author Name Book 2", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "HTML5 & CSS3 The complete Manua3", "Author Name Book 3", "/Content/Images/book3.jpg"));
+            //check if not exit
+            foreach (Book b in books)
+            {
+                if (b.Id == Id)
+                {
+                    books.Remove(b);
+                    break;
+                }
+            }
+            return View("ListBookModel", books);
+        }
+
+    }
+}
---
+++ Lab2/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab2.Models
+{
+    public class Book
+    {
+
+        private int id;
+        private string title;
+        private string author;
+        private string image_cover;
+
+        public Book() { }
+
+        public Book(int id, string title, string author, string image_cover) {
+
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.image_cover = image_cover;
+            
+        }
+        [Required(ErrorMessage ="Id không được trống")]
+        [Display(Name = "Mã sách")]
+        public int Id { get { return id; }  set { id = value; } }
+        public string Title { get { return title; } set { title = value; } }
+        public string Author { get { return author; } set { author = value; } }
+        public string Imagecover { get { return image_cover; } set { image_cover = value; } }
+    }
+}
